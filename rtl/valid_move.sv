506161062b2a3fa6eceb4e209b6c2973d1d28d0f
mtkaz1 <188244819+mtkaz1@users.noreply.github.com>
2026-09-02 10:07:11 -0600

    rtl change: modified valid_move.sv

--- rtl/valid_move.sv
+++ rtl/valid_move.sv
@@ -7,15 +7,15 @@ module valid_move( input logic [3:0] position,
                     always_comb
                         begin
                             case(position)
-                                4'b0000: valid = (board_state[1:0] == 2'b00) ? 1'b1 : 1'b0; //block 1 on the board
-                                4'b0001: valid = (board_state[3:2] == 2'b00) ? 1'b1 : 1'b0; //block 2 on the board
-                                4'b0010: valid = (board_state[5:4] == 2'b00) ? 1'b1 : 1'b0; //block 3 on the board
-                                4'b0011: valid = (board_state[7:6] == 2'b00) ? 1'b1 : 1'b0; //block4 on the board
-                                4'b0100: valid = (board_state[9:8] == 2'b00) ? 1'b1 : 1'b0; //block 5 on the board
-                                4'b0101: valid = (board_state[11:10] == 2'b00) ? 1'b1 : 1'b0; //block 6 on the board
-                                4'b0110: valid = (board_state[13:12] == 2'b00) ? 1'b1 : 1'b0; //block 7 on the board
-                                4'b0111: valid = (board_state[15:14] == 2'b00) ? 1'b1 : 1'b0; //block 8 on the board
-                                4'b1000: valid = (board_state[17:16] == 2'b00) ? 1'b1 : 1'b0; //block 9 on the board
+                                4'b0000: valid = (board_state[1:0] == 2'b00); //block 1 on the board
+                                4'b0001: valid = (board_state[3:2] == 2'b00); //block 2 on the board
+                                4'b0010: valid = (board_state[5:4] == 2'b00); //block 3 on the board
+                                4'b0011: valid = (board_state[7:6] == 2'b00); //block4 on the board
+                                4'b0100: valid = (board_state[9:8] == 2'b00); //block 5 on the board
+                                4'b0101: valid = (board_state[11:10] == 2'b00); //block 6 on the board
+                                4'b0110: valid = (board_state[13:12] == 2'b00); //block 7 on the board
+                                4'b0111: valid = (board_state[15:14] == 2'b00); //block 8 on the board
+                                4'b1000: valid = (board_state[17:16] == 2'b00); //block 9 on the board
                                 default: valid = 1'b0;
                             endcase
 endmodule
